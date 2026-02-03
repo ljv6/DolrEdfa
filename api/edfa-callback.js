d99ae310b07d19833acb4d0e9a70860cb0e91c27
ljv6 <maxmohamedmoon@gmail.com>
2026-02-03 03:41:27 +0300

    Add POST handler for EdfaPay callback

--- api/edfa-callback.js
+++ api/edfa-callback.js
@@ -1,1 +1,11 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed');
+  }
 
+  const data = req.body;
+
+  console.log('EdfaPay Callback:', data);
+
+  return res.status(200).json({ status: 'received' });
+}
